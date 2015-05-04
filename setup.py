a734ec717ded6730f5159f10802fea83784a4d37
Michel Käser <mail@michelkaeser.ch>
2015-05-04 18:05:01 +0200

    fix pep8 warnings

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name = "ipynbsrv-contract",
-    description = "Contract package for various pluggable ipynbsrv components.",
-    version = "0.0.1",
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    namespace_packages = ['ipynbsrv']
+    name="ipynbsrv-contract",
+    description="Contract package for various pluggable ipynbsrv components.",
+    version="0.0.1",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['ipynbsrv']
 )
